64dc1c8f655a8a1d11405fee691bac4b360b3f16
JamesMAM <jamesmam@hotmail.com>
2022-06-29 04:07:48 -0500

    primer comit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -2,7 +2,7 @@
 
 // Crea una variable "string", puede contener lo que quieras:
 const nuevaString = null;
-
+hola
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = null;
 
